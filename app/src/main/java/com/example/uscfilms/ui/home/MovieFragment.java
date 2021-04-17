afdf38ad52affc1d7b4a38559e3db6920c37d460
Monologue.№ <cancan@usc.edu>
2021-04-17 16:33:49 -0700

    fix async

--- app/src/main/java/com/example/uscfilms/ui/home/MovieFragment.java
+++ app/src/main/java/com/example/uscfilms/ui/home/MovieFragment.java
@@ -29,13 +29,16 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.uscfilms.R;
 import com.example.uscfilms.adapter.RecyclerViewDataAdapter;
+import com.example.uscfilms.adapter.SliderAdapter;
 import com.example.uscfilms.model.CardList;
 import com.example.uscfilms.model.SingleCard;
+import com.example.uscfilms.model.SliderData;
 import com.example.uscfilms.service.NowPlayingMovies;
 import com.example.uscfilms.service.PopularMovies;
 import com.example.uscfilms.service.TopRatedMovies;
 import com.example.uscfilms.service.VolleyCallback;
 import com.google.gson.JsonArray;
+import com.smarteist.autoimageslider.SliderView;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
@@ -52,6 +55,7 @@ public class MovieFragment extends Fragment {
     private final static String TAG = "MovieFragment";
     TextView textView;
     JSONArray arr;
+    String dummyURL = "https://image.tmdb.org/t/p/w500/inJjDhCjfhh3RtrJWBmmDqeuSYC.jpg";
 
     ArrayList<CardList> allCardLists;
 
@@ -63,24 +67,85 @@ public class MovieFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         allCardLists = new ArrayList<CardList>();
+
     }
 
     private void createNowPlayingMovies(Context cxt, View view) {
         NowPlayingMovies npm = new NowPlayingMovies();
+
+        // Urls of our images.
+        String url1 = "https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200-1.png";
+        String url2 = "https://qphs.fs.quoracdn.net/main-qimg-8e203d34a6a56345f86f1a92570557ba.webp";
+        String url3 = "https://bizzbucket.co/wp-content/uploads/2020/08/Life-in-The-Metro-Blog-Title-22.png";
+        // we are creating array list for storing our image urls.
+        ArrayList<SliderData> sliderDataArrayList = new ArrayList<>();
+
+        // initializing the slider view.
+        SliderView sliderView = view.findViewById(R.id.slider);
+
         npm.getNowPlayingMovies(new VolleyCallback() {
             @Override
             public void onSuccess(JSONArray res) throws JSONException {
                 for (int i = 0; i < 6; i++) {
                     String imgURL = "https://image.tmdb.org/t/p/w500" + res.getJSONObject(i).getString("backdrop_path");
-                    ImageView image = view.findViewById(R.id.imageView);
-                  Picasso.get().load(imgURL).into(image);
+                    sliderDataArrayList.add(new SliderData(imgURL));
                 }
+                // passing this array list inside our adapter class.
+                SliderAdapter adapter = new SliderAdapter(cxt, sliderDataArrayList);
+
+                // below method is used to set auto cycle direction in left to
+                // right direction you can change according to requirement.
+                sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
+
+                // below method is used to
+                // setadapter to sliderview.
+                sliderView.setSliderAdapter(adapter);
+
+                // below method is use to set
+                // scroll time in seconds.
+                sliderView.setScrollTimeInSec(3);
+
+                // to set it scrollable automatically
+                // we use below method.
+                sliderView.setAutoCycle(true);
+
+                // to start autocycle below method is used.
+                sliderView.startAutoCycle();
+
             }
         }, cxt);
+
+
+//        // adding the urls inside array list
+//        sliderDataArrayList.add(new SliderData(url1));
+//        sliderDataArrayList.add(new SliderData(url2));
+//        sliderDataArrayList.add(new SliderData(url3));
+//
+//        // passing this array list inside our adapter class.
+//        SliderAdapter adapter = new SliderAdapter(cxt, sliderDataArrayList);
+//
+//        // below method is used to set auto cycle direction in left to
+//        // right direction you can change according to requirement.
+//        sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
+//
+//        // below method is used to
+//        // setadapter to sliderview.
+//        sliderView.setSliderAdapter(adapter);
+//
+//        // below method is use to set
+//        // scroll time in seconds.
+//        sliderView.setScrollTimeInSec(3);
+//
+//        // to set it scrollable automatically
+//        // we use below method.
+//        sliderView.setAutoCycle(true);
+//
+//        // to start autocycle below method is used.
+//        sliderView.startAutoCycle();
     }
 
 
-    private void createTopRatedMovies(Context cxt, View view) throws JSONException {
+    private void createTopRatedMovies(Context cxt, View view) {
         CardList topRatedMoviesList = new CardList();
         topRatedMoviesList.setSubtitle("Top-Rated");
         ArrayList<SingleCard> singleCard = new ArrayList<SingleCard>();
@@ -91,18 +156,26 @@ public class MovieFragment extends Fragment {
                 for (int i = 0; i < 20; i++) {
                     String imgURL = "https://image.tmdb.org/t/p/w500" + res.getJSONObject(i).getString("backdrop_path");
 //                    ImageView image = view.findViewById(R.id.imageView);
-//                    Picasso.get().load(String.valueOf(imgURL)).into(image);
+//                    Picasso.get().load(imgURL).into(image);
+                    Log.d(TAG, "onSuccess1" + imgURL);
 
                     singleCard.add(new SingleCard("Id " + i, imgURL, res.getJSONObject(i).getString("title")));
                 }
                 topRatedMoviesList.setCardList(singleCard);
                 allCardLists.add(topRatedMoviesList);
-                Log.d(TAG, "cb " + allCardLists);
+                Log.d(TAG, "cb, top-rated " + singleCard);
             }
         }, cxt);
+
+        // try dummy data
+//        for (int j = 0; j < 20; j++) {
+//            singleCard.add(new SingleCard(String.valueOf(j), dummyURL, String.valueOf(j)));
+//        }
+//        topRatedMoviesList.setCardList(singleCard);
+//        allCardLists.add(topRatedMoviesList);
     }
 
-    private void createPopularMovies(Context cxt, View view) throws JSONException {
+    private void createPopularMovies(Context cxt, View view)  {
         CardList popularMoviesList = new CardList();
         popularMoviesList.setSubtitle("Popular");
         ArrayList<SingleCard> singleCard = new ArrayList<SingleCard>();
@@ -113,13 +186,19 @@ public class MovieFragment extends Fragment {
                 for (int i = 0; i < 20; i++) {
                     String imgURL = "https://image.tmdb.org/t/p/w500" + res.getJSONObject(i).getString("backdrop_path");
 //                    ImageView image = view.findViewById(R.id.imageView);
-//                    Picasso.get().load(String.valueOf(imgURL)).into(image);
+//                    Picasso.get().load(imgURL).into(image);
 
                     singleCard.add(new SingleCard("Id " + i, imgURL, res.getJSONObject(i).getString("title")));
                 }
                 popularMoviesList.setCardList(singleCard);
                 allCardLists.add(popularMoviesList);
-                Log.d(TAG, "cb " + allCardLists);
+                Log.d(TAG, "cb, popular " + popularMoviesList);
+                // dummy data
+                RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.movie_recycler_view);
+                recyclerView.setHasFixedSize(true);
+                RecyclerViewDataAdapter adapter = new RecyclerViewDataAdapter(getContext(), allCardLists);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+                recyclerView.setAdapter(adapter);
             }
         }, cxt);
     }
@@ -133,26 +212,8 @@ public class MovieFragment extends Fragment {
 
         Context cxt = getActivity();
         createNowPlayingMovies(cxt, view);
-        try {
-            createTopRatedMovies(cxt, view);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-            createPopularMovies(cxt, view);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        // dummy data
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.movie_recycler_view);
-        recyclerView.setHasFixedSize(true);
-        RecyclerViewDataAdapter adapter = new RecyclerViewDataAdapter(getContext(), allCardLists);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        recyclerView.setAdapter(adapter);
-
-
-
+        createTopRatedMovies(cxt, view);
+        createPopularMovies(cxt, view);
 
         return view;
 
