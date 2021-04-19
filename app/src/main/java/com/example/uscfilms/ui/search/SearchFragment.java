3dbadd3cc9774c010ed37cb6b42bcfb91638802f
Monologue.№ <cancan@usc.edu>
2021-04-18 22:08:38 -0700

    changed searchView color

--- app/src/main/java/com/example/uscfilms/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/uscfilms/ui/search/SearchFragment.java
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.example.uscfilms.R;
@@ -22,6 +24,7 @@ public class SearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
 //    @Override
@@ -34,6 +37,14 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+//        SearchView searchView = view.findViewById(androidx.appcompat.R.id.search_bar));
+//
+//
+//        searchView.setHintTextColor(getResources().getColor(R.color.white));
+//        searchView.setTextColor(getResources().getColor(R.color.white));
+
+        return view;
     }
 }
